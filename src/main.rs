4458188634d707d9b07e7ddc11e133759352412a
yguenduez <yasinguenduez1@gmail.com>
2023-04-26 16:50:45 +0200

    add tokio hello world from axum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
 mod business_logic;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        // Hyper server takes a make service.
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, world!"
 }
