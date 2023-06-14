b6dd6430ba393fb9dbc7a176c2ced7c1115d8c6c
yguenduez <yasinguenduez1@gmail.com>
2023-06-14 21:16:17 +0200

    add KeyValue Storage delete logic

--- src/key_value_storage.rs
+++ src/key_value_storage.rs
@@ -20,8 +20,9 @@ impl Storage for KeyValueStorage {
         }
     }
 
-    fn delete_pokemon(&self, id: usize) -> Result<(), StorageError> {
-        todo!()
+    fn delete_pokemon(&mut self, id: usize) -> Result<(), StorageError> {
+        self.inner.remove(&id);
+        Ok(())
     }
 
     fn update_pokemon(&self, pokemon: Pokemon) -> Result<(), crate::storage::StorageError> {
@@ -107,5 +108,27 @@ mod tests {
         // then
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), storage::StorageError::NotFound);
+    }
+
+    #[test]
+    fn delete_pokemon_given_id_with_when_called_with_pokemon_then_deletes_it() {
+        // given
+        let id = 6;
+        let mut storage = HashMap::new();
+        storage.insert(
+            id,
+            Pokemon {
+                name: "Glumanda".to_string(),
+                id,
+            },
+        );
+        let mut storage = KeyValueStorage::with(storage);
+
+        // when
+        let result = storage.delete_pokemon(id);
+
+        // then
+        assert!(result.is_ok());
+        assert_eq!(storage.inner.get(&id), None);
     }
 }
